851d0dd294336f20cf53f30df68501a3255d8dc5
nom <91125808+Cannelle1980@users.noreply.github.com>
2022-05-17 15:35:47 +0200

    Météo

--- météo/script.js
+++ météo/script.js
@@ -1,14 +1,45 @@
+const key = "42f91f35de918af0baacb8a480b60482";
+let button = document.getElementById("button");
+button.addEventListener("click", function(e) {
+    e.preventDefault();
+    //let input = document.getElementById("search");
+    let enWeather = ask();
+    //document.getElementById("result").innerHTML(weather);
+});
 
-const fetch = require("node-fetch");
-const apikey="106632d15bffa83b0512b461a6fc5ae3"
-let city = "london"
-let coutry ="uk"
-
-function wheather (city){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+ city +',uk&APPID='+apikey).then(Response =>{
-        return Response.json()
+function fetchApi(lat,lon) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+key+"&lang=FR")
+    .then(response => response.json())
+    .then(data => {
+        getFrenchWeather(data.weather[0].description);
     })
-
+    .catch(error=>{
+        console.log("erreur : "+ error);
+    })
+    ;
 }
 
-console.log(wheather(city));
+function getFrenchWeather(wheather) {
+    let frenchReturn = "";
+    if(wheather === "clear sky") {
+        frenchReturn = "Ciel clair";
+    }
+    
+    return frenchReturn;
+
+}
+function ask (){
+    let country = document.getElementById("ville").value;
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q="+ country +"&limit=5&appid="+key)
+    .then(response => response.json())
+    .then(data => {
+        fetchApi(data[0].lat,data[0].lon);
+    })
+    .catch(error=>{
+        console.log("erreur la ville n'existe pas : "+ error);
+    })
+    ;
+
+
+    
+}
