91b2b085e8cb5a98cb872f5f8d08c3113e742770
nom <91125808+Cannelle1980@users.noreply.github.com>
2022-05-17 17:28:33 +0200

    meteo fonctionnelle

--- météo/script.js
+++ météo/script.js
@@ -2,16 +2,14 @@ const key = "42f91f35de918af0baacb8a480b60482";
 let button = document.getElementById("button");
 button.addEventListener("click", function(e) {
     e.preventDefault();
-    //let input = document.getElementById("search");
-    let enWeather = ask();
-    //document.getElementById("result").innerHTML(weather);
+    ask();
+    
 });
-
 function fetchApi(lat,lon) {
     fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+key+"&lang=FR")
     .then(response => response.json())
     .then(data => {
-        diplay(data.weather[0].description);
+        display(data.weather[0].description ,data.main.temp);
     })
     .catch(error=>{
         console.log("erreur : "+ error);
@@ -29,7 +27,13 @@ function ask (){
         console.log("erreur la ville n'existe pas : "+ error);
     })
     ;
+}
+function display (finalWheather,Temp){
+    Temp= Math.round(Temp - 273.15)
+    //console.log(finalWheather,);
+    let displayTemp=Temp +"°";
+    let displayWheather=finalWheather;
+    document.getElementById("Temp").innerHTML=displayTemp;
+    document.getElementById("finalWheather").innerHTML=displayWheather;
 
-
-    
 }
