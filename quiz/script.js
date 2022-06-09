886ba87125d66eb19cec118c124ac0329a66aab6
nom <91125808+Cannelle1980@users.noreply.github.com>
2022-06-09 15:51:47 +0200

    bug corrigé quiz

--- quiz/script.js
+++ quiz/script.js
@@ -1,34 +1,34 @@
-var count =0;
+var count =0; //score quiz a point negatif
 function valider() { //verifie les bonnes réponses un point en plus si juste un point en moins si faux
     if (document.getElementById("Q1").checked && document.getElementById("Q2").checked && !document.getElementById("Q3").checked) {
         count++;
     } else { 
         count--;
         }
-    if(document.getElementById("Q4").checked){
+    if(document.getElementById("Q4").checked){ //question deux
         count++;
     } else{
         count--;  
     }
-    if(document.getElementById("Q5").checked){
+    if(document.getElementById("Q5").checked){ //question 3
         count ++;
     } else{
         count--;
     }
-    if(document.getElementById("Q6").checked){
+    if(document.getElementById("Q6").checked && !document.getElementById("Q7").checked && !document.getElementById("Q8").checked){ //question 4
         count ++;
     } else{
         count--;
     }
-    if(document.getElementById("Q9").checked && document.getElementById("Q10").checked){
+    if(document.getElementById("Q9").checked && document.getElementById("Q10").checked && !document.getElementById("Q11").checked){ //question 5
         count ++;
     } else{
         count--;
     }
-    if (count < 3){
+    if (count < 3){ // score final affiche le resultat et un commentaire dans une fenetre pop up
         alert("Vous avez" + " "+ count+" " +"passable !!!")
     }else{
         alert("Vous avez" + " "+ count+" " +"points c'est bien !!!") 
     }
-    count =0;
+    count =0; //score rénitialiser
     }
