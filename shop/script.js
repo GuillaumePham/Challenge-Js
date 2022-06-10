4e05ef16f37ad43138f213288a03fac0fd065c2e
nom <91125808+Cannelle1980@users.noreply.github.com>
2022-06-10 21:23:07 +0200

    commentaire

--- shop/script.js
+++ shop/script.js
@@ -1,17 +1,17 @@
-let shopping = document.getElementById("shop"); //
-let shopbag = document.getElementById("shopping-basket");
+let shopping = document.getElementById("shop"); //le shop
+let shopbag = document.getElementById("shopping-basket"); //le panier
 shopbag.classList.add("none");
 shopping.classList.add("block");
-let AirForceShop = document.getElementsByClassName("AirForceShop");
-let JordanBuy = document.getElementsByClassName("JordanBuy"); //nb de jordan acheté
-for (i = 0; i < AirForceShop.length; i++){
+let AirForceShop = document.getElementsByClassName("AirForceShop"); //tout les element de l'interface du panier
+let JordanBuy = document.getElementsByClassName("JordanBuy"); 
+for (i = 0; i < AirForceShop.length; i++){ //permet de passer d'une interface a l'autre
     AirForceShop[i].classList.add("none");
 }
 for (y = 0; y < JordanBuy.length; y++){
     JordanBuy[y].classList.add("none");
 }
-var n = localStorage.getItem('AirForce');
-var r = localStorage.getItem('Jordan');
+var n = localStorage.getItem('AirForce'); // nombre de air force acheté
+var r = localStorage.getItem('Jordan'); // pareil mais jordan
 let empty = document.getElementById("empty");
 empty.classList.add("none");
 let Total;
@@ -31,7 +31,7 @@ function Jordan(){
     ++r;
     return localStorage.setItem("Jordan", r);
 }
-function lostJordanBuy(){
+function lostJordanBuy(){//retire element panier
     --r;
     if (r <= 0){
         localStorage.removeItem("Jordan");
@@ -40,7 +40,7 @@ function lostJordanBuy(){
     localStorage.setItem("Jordan", r);
     window.location.reload(true);
 }
-function lostAirForceBuy(){
+function lostAirForceBuy(){ //retire element panier
     --n;
     if (n <= 0){
        localStorage.removeItem("AirForce");
@@ -49,20 +49,20 @@ function lostAirForceBuy(){
     localStorage.setItem("AirForce", n);
     window.location.reload(true);
 }
-function lostall(){
+function lostall(){ //retire  tout les elements du panier
     localStorage.removeItem("AirForce");
     localStorage.removeItem("Jordan");
     window.location.reload(true);
 }
 function priceAirForceBuy(){
-    if (n == null || n <= 0){
+    if (n == null || n <= 0){ //pas d'air force acheté
         for (i = 0; i < AirForceShop.length; i++){
             AirForceShop[i].classList.add("none");
         }
         n=0;
         return document.getElementById('priceAirForceBuy').innerHTML = "";
     }
-    if (n!= 0 || n != null){
+    if (n!= 0 || n != null){ //elles sont achetés affiche leurs nombres et le prix du lot
         AirForceBuyPrice = 250 * n;
         for (i = 0; i < AirForceShop.length; i++){
             AirForceShop[i].classList.remove("none");
@@ -94,7 +94,7 @@ function CheckIfItem(){
         document.getElementById('priceJordan').innerHTML += shopping;
     }
 }
-function bag(){
+function bag(){ //verifie le panier et affiche le panier de l'acheteur
     if (n == null && r == null || n == 0 && r == 0 || n == 0 && r == null || n == null && r==0){
         
         alert("Panier Vide !");
@@ -103,7 +103,7 @@ function bag(){
     shopbag.classList.toggle("none");
     document.getElementById('counterAirForceBuy').innerHTML = n;  
     document.getElementById('counterJordan').innerHTML = r; 
-    priceAirForceBuy(); 
+    priceAirForceBuy();//appelle les fonctions pour les achat de chaussures leurs prix et nb
     priceJordan(); 
     if (JordanPrice == null){
         JordanPrice = 0;
